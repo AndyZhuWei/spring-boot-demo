3f2500d8ffa2d942b15d4cf60d3ea481a1b1cfcb
zhuwei <weizhu@healthlink.cn>
2018-10-25 18:07:04 +0800

    Spring boot

---
+++ spring-boot/src/main/java/cn/andy/springBoot/Application.java
@@ -0,0 +1,44 @@
+package cn.andy.springBoot;
+
+import cn.andy.springBoot.cn.andy.springBoot.config.AuthorSettings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@SpringBootApplicaiton是Spring Boot项目的核心注解，主要目的是开启自动配置。
+@SpringBootApplication
+//可以通过@ImportResource的方式导入xml配置文件
+//@ImportResource({"classpath:xx","classpath:yy"})
+public class Application {
+
+	@Value("${book.author}")
+	private String bookAuthor;
+
+	@Value("${book.name}")
+	private String bookName;
+
+	@Autowired
+	private AuthorSettings authorSettings;
+
+	@RequestMapping("/")
+	String index() {
+//		return "Hello Spring Boot";
+//		return "book name is:"+bookName+" and book author is:"+bookAuthor;
+		return "author name is:"+authorSettings.getName()+" and author age is:"+authorSettings.getAge();
+	}
+
+
+	//这是一个标准的Java应用的main方法，主要作用是作为项目启动的入口
+	public static void main(String[] args) {
+		//SpringApplication.run(Application.class, args);
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
+	}
+}
--- spring-boot/src/main/java/cn/andy/springBoot/cn/andy/springBoot/config/AuthorSettings.java
+++ spring-boot/src/main/java/cn/andy/springBoot/cn/andy/springBoot/config/AuthorSettings.java
@@ -1,9 +1,34 @@
 package cn.andy.springBoot.cn.andy.springBoot.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
 /**
  * @Author: zhuwei
  * @Date:2018/10/25 17:38
  * @Description:
  */
+@Component
+//通过@ConfigurationProperties加载properties文件的配置，prefix属性指定properties的配置的前缀，
+//通过locations指定properties文件的位置
+@ConfigurationProperties(prefix = "author")
 public class AuthorSettings {
+    private String name;
+    private Long age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
 }
